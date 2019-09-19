329f7c70ebfb7278eee0485d42a39387fa4b072a
nasyiqin <55252513+nasyiqin@users.noreply.github.com>
2019-09-19 17:53:12 +0800

    version1

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+
+    public static void main(String [] syiqin) {
+
+        int A = (6*3)/2;
+        int B = (4*3)/2;
+        int C = 4*3;
+
+        System.out.println("A= " + A);
+        System.out.println("B= " + B);
+        System.out.println("C= " + C);
+        System.out.println("Total= " + (A+B+C));
+    }
+}
